2e6dd50b89c8f9e1c06ec229a90e94ab649cbac0
LunaDiAmb <ddiambrosio7467@floridapoly.edu>
2025-05-12 17:49:56 +0000

    created file

---
+++ week1/first_program/first_program.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// "using" directive
+// using namespace std;
+// using std::cout;
+
+int main()
+{
+    // print welcome message
+    std::cout << "hello" << std::endl;
+
+    std::cout << "5 / 2 is " << 5.0 / 2 << std::endl;
+
+    // cout - console output
+    // << - stream insertion operator
+    // :: - scope resolution operator
+    // std - standard namespace
+
+
+    return 0;
+}
